0eea22319e4e90e3f7dc575e2efa0574d135849c
anarcher <anarcher@gmail.com>
2017-02-01 23:57:25 +0900

    Add LogAdater

--- gokitlog.go
+++ gokitlog.go
@@ -4,7 +4,6 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-kit/kit/log"
 )
 
 // ErrorLogger returns an ErrorLoggerT with parameter gin.ErrorTypeAny
@@ -27,28 +26,27 @@ func ErrorLoggerT(typ gin.ErrorType) gin.HandlerFunc {
 	}
 }
 
-// Logger prints a logline for each request and measures the time to
-// process for a call. It formats the log entries similar to
+// LogAdapter returns a new LogAdapter wrapper around the passed
+// logger. It formats the log entries similar to
 // http://godoc.org/github.com/gin-gonic/gin#Logger does.
 //
 // Example:
 //        router := gin.New()
-//        router.Use(ginglog.Logger(logger))
-func Logger(plogger log.Logger) gin.HandlerFunc {
+//        router.Use(gokitlog.LogAdapter(func(c *gin.Context){
+//				 latency,_ := c.Get("latency")
+//               logger.Log(
+//					"latency",latency,"clientIP",c.ClientIP(),
+//					"status",c.Writer.Status(),"err",c.Errors.String())
+//		  })
+func LogAdapter(fn func(c *gin.Context)) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		logger := log.NewContext(plogger)
 		t := time.Now()
 
 		// process request
 		c.Next()
 
 		latency := time.Since(t)
-		clientIP := c.ClientIP()
-		method := c.Request.Method
-		statusCode := c.Writer.Status()
-		path := c.Request.URL.Path
-
-		logger.Log("latency", latency, "clientIP", clientIP, "status", statusCode, "method", method, "path", path, "err", c.Errors.String())
-
+		c.Set("latency", latency)
+		fn(c)
 	}
 }
--- gokitlog_test.go
+++ gokitlog_test.go
@@ -4,13 +4,18 @@ import (
 	"os"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/go-kit/kit/log"
 )
 
 func Test_Logger(t *testing.T) {
 	logger := log.NewLogfmtLogger(os.Stderr)
 	logger = log.NewContext(logger).With("ts", log.DefaultTimestampUTC)
-	middleware := Logger(logger)
+
+	middleware := LogAdapter(func(c *gin.Context) {
+		latency, _ := c.Get("latency")
+		logger.Log("a", "b", "latency", latency)
+	})
 
 	if middleware == nil {
 		t.Errorf("Can't get Logger middleware")
